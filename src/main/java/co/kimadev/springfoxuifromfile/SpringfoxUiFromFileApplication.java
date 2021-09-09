ad9a1a6b6209449dd26b986dd9650d4784a436b1
Raul.Lima-consultorexterno <raul.lima-consultorexterno@rabobank.com>
2021-09-09 18:13:33 -0300

    Add project structure

---
+++ src/main/java/co/kimadev/springfoxuifromfile/SpringfoxUiFromFileApplication.java
@@ -0,0 +1,13 @@
+package co.kimadev.springfoxuifromfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfoxUiFromFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringfoxUiFromFileApplication.class, args);
+	}
+
+}
---
+++ src/main/java/co/kimadev/springfoxuifromfile/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package co.kimadev.springfoxuifromfile.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Primary
+    @Bean
+    public SwaggerResourcesProvider swaggerResourcesProvider() {
+        return () -> {
+            SwaggerResource wsResource = new SwaggerResource();
+            wsResource.setName("Documentation");
+            wsResource.setSwaggerVersion("2.0");
+            wsResource.setLocation("/swagger.yaml");
+
+            List<SwaggerResource> resources = List.of(wsResource);
+            return resources;
+        };
+    }
+}
+
